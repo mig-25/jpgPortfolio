b3651541d0af158b07ef2fa663adf8d34538f9a4
mig-25 <you@example.com>
2020-10-05 14:02:37 +0200

    add 404, contact, footer

--- student_frontend/src/pages/contact.js
+++ student_frontend/src/pages/contact.js
@@ -1,8 +1,42 @@
 import React from "react"
 import Layout from "../components/Layout"
+import SEO from "../components/SEO"
 
 const contact = () => {
-  return <h2>contact page</h2>
+  return (
+    <Layout>
+      <section className="contact-page">
+        <article className="contact-form">
+          <h3>kontakta mig</h3>
+          <form action="https://formspree.io/xnqgokpo" method="POST">
+            <div className="form-group">
+              <input
+                type="text"
+                placeholder="namn"
+                name="name"
+                className="form-control"
+              />
+              <input
+                type="email"
+                placeholder="epost"
+                name="email"
+                className="form-control"
+              />
+              <textarea
+                name="message"
+                rows="5"
+                placeholder="meddelande"
+                className="form-control"
+              ></textarea>
+            </div>
+            <button type="submit" className="submit-btn btn">
+              Kontakta mig
+            </button>
+          </form>
+        </article>
+      </section>
+    </Layout>
+  )
 }
 
 export default contact
